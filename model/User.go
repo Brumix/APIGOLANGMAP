5a1fb8479c90a2ba8cfb564b5fa3daca0c4c995b
André Filipe <andrefilipe182000@gmail.com>
2022-04-05 22:38:22 +0100

    base database model

--- model/User.go
+++ model/User.go
@@ -3,7 +3,9 @@ package model
 import "gorm.io/gorm"
 
 type User struct {
-	gorm.Model `swaggerignore:"true"`
-	Username   string `json:"username" gorm:"unique"`
-	Password   string `json:"password,omitempty"`
+	gorm.Model    `swaggerignore:"true"`
+	Username      string     `json:"username" gorm:"unique"`
+	Password      string     `json:"password,omitempty"`
+	UserFriends   []Follower `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	UserPositions []Position `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
