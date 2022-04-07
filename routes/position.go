317053db7e73e2d6d481dc716ff5b7206e8cbe46
Brumix <brunompereira42@gmail.com>
2022-04-07 01:43:09 +0100

    [CONFLICT] resolved Conflicts

--- routes/position.go
+++ routes/position.go
@@ -11,17 +11,4 @@ func RegisterLocation(c *gin.Context) {
 }
 func DeleteLocation(c *gin.Context) {
 	controllers.DeleteLocation(c)
-
-  
-// @Summary Obtem os Followers
-// @Description Exibe a lista, sem todos os campos, de todas as avaliações
-// @Accept  json
-// @Produce  json
-// @Security BearerAuth
-// @param Authorization header string true "Token"
-// @Success 200 {array} model.Evaluation
-// @Router /evaluation [get]
-// @Failure 404 "Not found"
-func GetAllFollowers(c *gin.Context) {
-	controllers.GetAllEvaluations(c)
 }
