fabecb6f6bdcfcb36a8fd629eda74e6c22e3c7d4
André Filipe <andrefilipe182000@gmail.com>
2022-04-07 15:41:37 +0100

    merged from staging

--- controllers/position.go
+++ controllers/position.go
@@ -17,18 +17,13 @@ func RegisterLocation(c *gin.Context) {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
 		return
 
-func GetAllPositions(c *gin.Context) {
-	var positions []model.Position
 	}
 
-	services.Db.Find(&positions)
 	if errStore := repo.StorePosition(&position); errStore != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
 		return
 	}
 
-	if len(positions) <= 0 {
-		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Empty list!"})
 	c.JSON(http.StatusOK, gin.H{"msg": "Position register with success!!",
 		"Position": position})
 	return
@@ -49,41 +44,4 @@ func DeleteLocation(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"msg": "Position deleted with success!!",
 		"Position": position})
 	return
-}
-
-	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": positions})
-}
-
-func GetPositionByUserId(c *gin.Context) {
-	var position model.Position
-	user_id := c.Param("user_id")
-
-	services.Db.First(&position, user_id)
-	if position.ID == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "User not found!"})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": position})
-}
-
-func AddPosition(c *gin.Context) {
-	var position model.Position
-
-	var userid, err = c.Get("userid")
-
-	if err == false {
-		c.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest, "message": "User Auth Token Malformed!"})
-		return
-	}
-
-	if err := c.ShouldBindJSON(&position); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest, "message": "Check syntax!"})
-		return
-	}
-
-	position.UserID = userid.(uint)
-
-	services.Db.Save(&position)
-	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "Create successful!", "resourceId": position.ID})
 }
--- routes/position.go
+++ routes/position.go
@@ -6,45 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// @Summary Recupera as posições de todos os users
-// @Description Exibe a lista, sem todos os campos, de todas as posições
-// @Accept  json
-// @Produce  json
-// @Security BearerAuth
-// @param Authorization header string true "Token"
-// @Success 200 {array} model.Position
-// @Router /position [get]
-// @Failure 404 "Not found"
-func GetAllPositions(c *gin.Context) {
-	controllers.GetAllPositions(c)
+//TODO SWAGGER
+func RegisterLocation(c *gin.Context) {
+	controllers.RegisterLocation(c)
 }
-
-// @Summary Recupera uma posição pelo id do user
-// @Description Exibe os detalhes da ultima posição do user através do seu id
-// @ID get-position-by-int
-// @Accept  json
-// @Produce  json
-// @Security BearerAuth
-// @param Authorization header string true "Token"
-// @Param id path int true "User ID"
-// @Success 200 {object} model.Position
-// @Router /position/{id} [get]
-// @Failure 404 "Not found"
-func GetPositionByUserId(c *gin.Context) {
-	controllers.GetPositionByUserId(c)
-}
-
-// @Summary Adicionar uma posição
-// @Description Cria uma posição sobre um utilizador
-// @Accept  json
-// @Produce  json
-// @Security BearerAuth
-// @param Authorization header string true "Token"
-// @Param position body model.Position true "Add position"
-// @Router /position [post]
-// @Success 201 {object} model.Position
-// @Failure 400 "Bad request"
-// @Failure 404 "Not found"
-func AddPosition(c *gin.Context) {
-	controllers.AddPosition(c)
+func DeleteLocation(c *gin.Context) {
+	controllers.DeleteLocation(c)
 }
