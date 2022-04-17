6743c9356376183cc898d6021d45358212712406
tmsl9 <55606395+tmsl9@users.noreply.github.com>
2022-04-17 23:40:43 +0100

    Update middlewares.go

--- services/middlewares.go
+++ services/middlewares.go
@@ -8,7 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
 func GinMiddleware(allowOrigin string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", allowOrigin)
